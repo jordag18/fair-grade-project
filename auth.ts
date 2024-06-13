970c93e11c6f99dfaead66d58f615e54083f6df3
Jordan A <jaaguon@miners.utep.edu>
2024-06-13 08:47:53 -0600

    Auth working

--- auth.ts
+++ auth.ts
@@ -1,6 +1,35 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
-})
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
+        }),
+        GitHubProvider({
+            clientId: process.env.AUTH_GITHUB_ID,
+            clientSecret: process.env.AUTH_GITHUB_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
+        })
+    ],
+});
