047919bfe8b676e03e54bffd18b6d1fff6b0103c
Jordan A <jaaguon@miners.utep.edu>
2024-06-16 15:17:45 -0600

    Configured Docker Compose and cloudbuild

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import LoginForm from "@/components/LoginForm";
 import Logout from "@/components/Logout";
 import { auth } from "@/auth";
-
 import { redirect } from "next/navigation";
 
 const HomePage = async () => {
@@ -10,12 +9,16 @@ const HomePage = async () => {
 
     if (!session?.user) redirect("/");
 
+    // Fallback image URL
+    const defaultImageUrl = "/path/to/default/image.jpg";
+    const userImage = session?.user?.image || defaultImageUrl;
+
     return (
         <div className="flex flex-col items-center m-4">
             <h1 className="text-3xl my-2">Welcome, {session?.user?.name}</h1>
             <Image
-                src={session?.user?.image}
-                alt={session?.user?.name}
+                src={userImage}
+                alt={session?.user?.name || "User"}
                 width={72}
                 height={72}
                 className="rounded-full"
