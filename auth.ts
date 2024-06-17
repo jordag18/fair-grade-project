437ec660ecfb6bd9902541ace6b830bcb4868cf7
Jordan A <jaaguon@miners.utep.edu>
2024-06-17 14:13:45 -0600

    Google Provider updated

--- auth.ts
+++ auth.ts
@@ -10,8 +10,8 @@ export const {
 } = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
             authorization: {
                 params: {
                     prompt: "consent",
