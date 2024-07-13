24877ea884e838115aca1403b0f0584ccb001081
Jordan A <jaaguon@miners.utep.edu>
2024-07-13 17:36:28 -0600

    Added User authentication and role check If user is authenticated at home page they are auto redirected to dashboard dashboard redirects user based on their role to their respective dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,15 @@
-"use client";
-import { signIn } from "next-auth/react";
-import { Button } from "@/components/ui/button";
-import { handleGoogleSignIn } from "@/lib/auth/googleSignInServerAction";
+import { redirect } from "next/navigation";
+import { SignInPage } from "./signin";
+import { checkIsAuthenticated } from "@/lib/auth/checkIsAuthenticated";
 
-export default function LoginPage() {
-  return (
-    <div className="flex h-screen bg-slate-300">
-      <div className="flex w-full p-24 pt-36">
-        <div className="w-1/2 bg-gray-100 p-8 flex flex-col justify-center items-center rounded-l-lg">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Welcome to Fair Grade
-          </h1>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Please sign in using one of the following methods to access your
-            account.
-          </p>
-        </div>
-        <div className="w-1/2 flex flex-col justify-center items-center bg-gray-200 p-8 rounded-r-lg">
-          <Button
-            onClick={() => handleGoogleSignIn()}
-            className="mb-4 w-2/3 bg-blue-600"
-          >
-            Sign in with Google
-          </Button>
-          <Button
-            onClick={() => signIn("github")}
-            className="mb-4 w-2/3 bg-slate-600"
-          >
-            Sign in with GitHub
-          </Button>
-          <Button
-            onClick={() => signIn("utep")}
-            className="w-2/3 bg-orange-600"
-          >
-            Sign in with UTEP (SSO)
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+const SignIn: React.FC = async () => {
+  const isAuthenticated = await checkIsAuthenticated();
+
+  if (isAuthenticated) {
+    redirect("/dashboard");
+  } else {
+    return <SignInPage />;
+  }
 }
+
+export default SignIn;
