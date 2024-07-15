7777285a6c451491d334a90d3252e72b467e514e
Jordan A <jaaguon@miners.utep.edu>
2024-07-15 13:34:50 -0600

    Modified .env variables and CI/CD files

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-    interface ProcessEnv {
-      DB_INSTANCE_CONNECTION: string;
-      DB_USER: string;
-      DB_PASSWORD: string;
-      DB_NAME: string;
-    }
-  }
-  
